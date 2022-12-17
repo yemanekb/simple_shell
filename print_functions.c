910f344e1d337696886cf71d2bc2a5f2ec2e6106
yemanekb <yemanekb@gmail.com>
2022-12-17 06:28:51 -0800

    prompt_user.c

--- print_functions.c
+++ print_functions.c
@@ -8,9 +8,7 @@
  */
 int _putchar(char c)
 {
-
-		return (write(1, &c, 1));
-
+	return (write(1, &c, 1));
 }
 
 /**
@@ -20,14 +18,12 @@ int _putchar(char c)
 */
 int print_s(char *s)
 {
-		int i = 0;
+	int i = 0;
 
-
-			while (s[i] != '\0')
-					{
-								_putchar(s[i]);
-										i++;
-											}
-
-				return (i);
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (i);
 }
