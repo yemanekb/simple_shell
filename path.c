4e374548df3dc86d03efb7c309d5bee0d1a99d05
yemanekb <yemanekb@gmail.com>
2022-12-17 06:00:04 -0800

    Print_env.c

--- path.c
+++ path.c
@@ -8,16 +8,20 @@ char *find_path(void)
 {
 	int x;
 	char **env = environ, *path = NULL;
+
 	while (*env)
 	{
-	if (_strncmp(*env, "PATH=", 5) == 0)
-											{
-												path = *env;
-												while (*path && x < 5)								
-												{path++;																			x++;												}
-																						return (path);
-																								}
-											env++;
-												}
-					return (NULL);
+		if (_strncmp(*env, "PATH=", 5) == 0)
+		{
+		path = *env;
+		while (*path && x < 5)
+		{
+			path++;
+			x++;
+		}
+		return (path);
+	}
+	env++;
+	}
+	return (NULL);
 }
