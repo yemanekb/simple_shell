7de3970aff795452cfb9d1ac6e80f75c9f1ee9a4
yemanekb <yemanekb@gmail.com>
2022-12-17 06:13:24 -0800

    print_function

--- print_env.c
+++ print_env.c
@@ -6,14 +6,13 @@
  */
 void print_env(void)
 {
-		int x = 0;
-			char **env = environ;
+	int x = 0;
+	char **env = environ;
 
-
-				while (env[x])
-						{
-									write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
-											write(STDOUT_FILENO, "\n", 1);
-													x++;
-														}
+	while (env[x])
+	{
+		write(STDOUT_FILENO, (const void *)env[x], _strlen(env[x]));
+		write(STDOUT_FILENO, "\n", 1);
+		x++;
+	}
 }
