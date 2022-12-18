7de3642205d1ff35c70c8a184e90dbf6144770f7
yemanekb <yemanekb@gmail.com>
2022-12-17 22:14:29 -0800

    tokenizer.c

--- test_path.c
+++ test_path.c
@@ -9,17 +9,16 @@
  */
 char *test_path(char **path, char *command)
 {
-		int i = 0;
-			char *output;
+	int i = 0;
+	char *output;
 
-
-				while (path[i])
-						{
-									output = append_path(path[i], command);
-											if (access(output, F_OK | X_OK) == 0)
-															return (output);
-													free(output);
-															i++;
-																}
-					return (NULL);
+	while (path[i])
+	{
+		output = append_path(path[i], command);
+		if (access(output, F_OK | X_OK) == 0
+			return (output);
+		free(output);
+		i++;
+		}
+		return (NULL);
 }
